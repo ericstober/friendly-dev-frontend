d2101d1a390b36c927382d9d5378be3538c87396
ericstober <erictstober@gmail.com>
2026-01-11 20:17:17 -0600

    add Featured Projects to home page

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,9 +1,28 @@
 import type { Route } from "./+types/index";
+import type { Project } from "~/types";
+import FeaturedProjects from "~/components/FeaturedProjects";
 
 export function meta({}: Route.MetaArgs) {
   return [{ title: "The Friendly Dev | Welcome" }, { name: "description", content: "Custom website development" }];
 }
 
-export default function Home() {
-  return <section>Home Page</section>;
+export async function loader({ request }: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
+
+  return { projects: data };
 }
+
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+
+  console.log(projects);
+
+  return (
+    <>
+      <FeaturedProjects projects={projects} count={2} />
+    </>
+  );
+};
+
+export default HomePage;
