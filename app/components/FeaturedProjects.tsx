c55828ac3914c87d17d71345ac774abdba8899b2
ericstober <erictstober@gmail.com>
2026-02-04 23:22:58 -0600

    remove featured filter from FeaturedProjects component, since the featured filter is now done in the api

--- app/components/FeaturedProjects.tsx
+++ app/components/FeaturedProjects.tsx
@@ -7,14 +7,12 @@ type FeaturedProjectsProps = {
 };
 
 const FeaturedProjects = ({ projects, count = 2 }: FeaturedProjectsProps) => {
-  const featured = projects.filter((p) => p.featured).slice(0, count);
-
   return (
     <section>
       <h2 className='text-2xl font-bold mb-6 text-gray-200'>Featured Projects</h2>
 
       <div className='grid gap-6 sm:grid-cols-2'>
-        {featured.map((project) => (
+        {projects.map((project) => (
           <ProjectCard key={project.id} project={project} />
         ))}
       </div>
