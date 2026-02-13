4eae163074d9d71f045ee36b5471535260660226
ericstober <erictstober@gmail.com>
2026-02-12 22:44:50 -0600

    fetch post details from strapi

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import type { Route } from "./+types";
-import type { PostMeta, StrapiResponse, StrapiPost } from "~/types";
+import type { Post, StrapiResponse, StrapiPost } from "~/types";
 import PostCard from "~/components/PostCard";
 import Pagination from "~/components/Pagination";
 import PostFilter from "~/components/PostFilter";
 
-export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Post[] }> {
   const res = await fetch(`${import.meta.env.VITE_API_URL}/posts?populate=image&sort=date:desc`);
 
   if (!res.ok) throw new Error("Failed to fetch data");
